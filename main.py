2980df38b5d64c96cc4e7829615ced2f13404005
stefan121212 <81379730+stefan121212@users.noreply.github.com>
2021-04-27 17:47:51 +0200

    Update main.py
    another test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 # from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
-from flask_gravatar import Gravatar
+# from flask_gravatar import Gravatar
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
