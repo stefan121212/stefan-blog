f99a1425b6b4e2b83e045e9eef58bc8e5865f98d
stefan121212 <81379730+stefan121212@users.noreply.github.com>
2021-04-27 17:51:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 # gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
-                    base_url=None)
+#                    base_url=None)
 
 # ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
